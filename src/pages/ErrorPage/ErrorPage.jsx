cd0dcf98dcc6bbfa35aa75380519767c30ce141f
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-06-05 17:11:27 +0300

    add syles error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,20 +1,22 @@
-import { Wrapper, Text, ImgWrapper, Text404 } from './ErrorPage.styles';
+import { Wrapper, Text } from './ErrorPage.styles';
 import { Container } from 'components/App/App.styles';
+import { useNavigate } from 'react-router-dom';
+import Button from 'components/shared/Button';
 import img404 from 'images/404-min.png';
 
-const ErrorPage = props => (
-  <section>
-    <Container>
-      <Text>Ooops! This page not found :(</Text>
-      <Wrapper>
-        <ImgWrapper>
-          <Text404>4</Text404>
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <section>
+      <Container>
+        <Text>Ooops! This page not found :(</Text>
+        <Wrapper>
           <img src={img404} alt="error 404" />
-          <Text404>4</Text404>
-        </ImgWrapper>
-      </Wrapper>
-    </Container>
-  </section>
-);
+          <Button text="to home" onClick={() => navigate('/')} />
+        </Wrapper>
+      </Container>
+    </section>
+  );
+};
 
 export default ErrorPage;
--- src/pages/ErrorPage/ErrorPage.styles.js
+++ src/pages/ErrorPage/ErrorPage.styles.js
@@ -12,25 +12,5 @@ export const Text = styled.p`
   color: ${props => props.theme.colors.text};
   font-size: 28px;
   font-weight: 500;
-  margin-bottom: 50px;
-`;
-
-export const ImgWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: ${props => props.theme.gradient.bg};
-  box-shadow: ${props => props.theme.shadows.default};
-  padding: 30px;
-  max-width: 250px;
-  max-height: 250px;
-  border-radius: 50%;
-`;
-
-export const Text404 = styled.p`
-  display: flex;
-  color: ${props => props.theme.colors.text};
-
-  font-size: 250px;
-  font-weight: 700;
+  margin-bottom: 10px;
 `;
